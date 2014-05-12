f047ff6d578f2c4f0d153ce15fffb31687427816
Owen van Dijk <ohwhen@users.noreply.github.com>
2014-05-12 17:58:33 +0200

    removed default UI

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 function initializeMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(52.3847296,4.8677805),
+        center: new google.maps.LatLng(52.3839441,4.8696277),
+        disableDefaultUI: true,
         zoom: 17
     };
     var map = new google.maps.Map(document.getElementById("gmaps"), mapOptions);
 }
 
 google.maps.event.addDomListener(window, 'load', initializeMap);
+
+//52.3847296,4.8677805
+
+//52.3836441,4.8696277
